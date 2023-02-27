e29597d67b0d0e4d3173768ed8cef35e0cbbec97
sydney-clark <sydney.natasha.clark@gmail.com>
2023-02-27 15:23:26 -0800

    commiting

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ const methodOverride = require('method-override');
 const trackerController = require('./controllers/classTracker');
 
 mongoose.set('strictQuery',true);
-mongoose.connect(process.env.MONGODB, () => {
-    console.log('The connection with mongodb is established at process.env.MONGODB');
+mongoose.connect('process.env.MONGODB', () => {
+    console.log('The connection with mongodb is established at ');
 })
-
-
-const app = express();
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method')); 
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public")); 
-
+// require('dotenv').config()
 app.use('/class_tracker', trackerController);
 
 // =======================
@@ -23,23 +20,24 @@ app.use('/class_tracker', trackerController);
 // =======================
 
 
-
-// mongoose.connect('mongodb://localhost:27017/tracker', () => {
-//     console.log('The connection with mongodb is established');
-// })
-
-
-
-
-
-////////////////////////////////////////////////////////////////
-//when connecting atlas and env use this
-////////////////////////////////////////////////////////////////
+mongoose.set('strictQuery',true);
+mongoose.connect('mongodb://localhost:27017/tracker', () => {
+    console.log('The connection with mongodb is established');
+})
 
 
 app.listen(3000, () => {
     console.log('listening...');
 })
+
+
+////////////////////////////////////////////////////////////////
+//when connecting atlas and env use this
+////////////////////////////////////////////////////////////////
+// mongoose.connect('process.env.MONGODB', () => {
+//     console.log('The connection with mongodb is established at ');
+// })
+
 // app.listen(3000, () => {
 //     console.log('listening...');
 // })
