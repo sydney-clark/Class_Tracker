4a6c71a751a4e52adf9fe4bc90ec3976e2e59d80
sydney-clark <sydney.natasha.clark@gmail.com>
2023-02-27 15:37:39 -0800

    css for index page updated

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const methodOverride = require('method-override');
 const trackerController = require('./controllers/classTracker');
 
 mongoose.set('strictQuery',true);
-mongoose.connect('process.env.MONGODB', () => {
-    console.log('The connection with mongodb is established at ');
+mongoose.connect(process.env.MONGODB, () => {
+    console.log('The connection with mongodb is established at process.env.MONGODB');
 })
+
+
+const app = express();
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method')); 
 app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public")); 
-// require('dotenv').config()
+
 app.use('/class_tracker', trackerController);
 
 // =======================
@@ -20,24 +23,23 @@ app.use('/class_tracker', trackerController);
 // =======================
 
 
-mongoose.set('strictQuery',true);
-mongoose.connect('mongodb://localhost:27017/tracker', () => {
-    console.log('The connection with mongodb is established');
-})
+
+// mongoose.connect('mongodb://localhost:27017/tracker', () => {
+//     console.log('The connection with mongodb is established');
+// })
 
 
-app.listen(3000, () => {
-    console.log('listening...');
-})
+
 
 
 ////////////////////////////////////////////////////////////////
 //when connecting atlas and env use this
 ////////////////////////////////////////////////////////////////
-// mongoose.connect('process.env.MONGODB', () => {
-//     console.log('The connection with mongodb is established at ');
-// })
 
+
+app.listen(3000, () => {
+    console.log('listening...');
+})
 // app.listen(3000, () => {
 //     console.log('listening...');
 // })
